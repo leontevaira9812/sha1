bcf5f9d96c853c9cadf7b23e9ecda65cba8f5485
Irina <48284966+leontevaira9812@users.noreply.github.com>
2021-12-16 18:46:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import multer from "multer";
+import sizeOf from "image-size";
+import sharp from "sharp";
+import fs from "fs";
+import axios from "axios";
+import crypto from "crypto";
+
+const app = express();
+
+const img = multer({
+  dest: "./img",
+});
+
+app
+  .use(express.urlencoded({ extended: true }))
+  .set("view engine", "ejs")
+  .set("views", "views")
+  .get("/", (r) => r.res.render("./index"))
+  .post("/size2json", img.single("image"), async (r) => {
+    const tempPath = r.file.path;
+    sizeOf(tempPath, function (err, dimensions) {
+      r.res.send({
+        width: dimensions.width,
+        height: dimensions.height,
+      });
+    });
+  })
+  .get("/makeimage?", (r) => {
+    const width = parseInt(r.query.width);
+    const height = parseInt(r.query.height);
+    sharp("./img/ALX_ICON.png")
+      .resize(width, height)
+      .toFile("./img/output.png", (err, info) => {
+        r.res.download("./img/output.png");
+      });
+  })
+  .get("/wordpress/", async (req, res, next) => {
+    const content = req.query.content;
+    const response = await axios.post(
+      "https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token",
+      { username: "gossjsstudent2017", password: "|||123|||456" }
+    );
+    const token = response.data.token;
+    const WPresponse = await axios.post(
+      `https://wordpress.kodaktor.ru/wp-json/wp/v2/posts/`,
+
+      { content, title: "alexisson", status: "publish" },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    res.send(WPresponse.data.id + "");
+  })
+  .all("/log", (r) => {
+    console.log(r.params.data);
+    console.log(r.headers);
+  })
+  .get("/sha1", (r) => {
+    r.res.render("./sha", { value: "" });
+  })
+  // .post("/sha1", (r) => {
+  //   r.res.render("./sha", {
+  //     value: crypto.createHash("sha1").update(r.body.inp).digest("hex"),
+  //   });
+  // })
+  .all("/login", (r) => r.res.send("alexisson"))
+  .listen(process.env.PORT || 3000, () => {
+    console.log("Server is working");
+  });
